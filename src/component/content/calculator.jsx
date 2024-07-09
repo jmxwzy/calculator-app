4d5e1613b6d454b5f5580bb983be5687a74e1812
jmxwzy <1140312750@qq.com>
2024-07-09 22:54:36 +0800

    计算器组件ui

--- src/component/content/calculator.jsx
+++ src/component/content/calculator.jsx
@@ -7,7 +7,30 @@ class Calculator extends Component {
         return (
             <React.Fragment>
                 <Base>
-                    计算器
+                    <div className='calculator'>
+                        <div className='output'>
+                            <div className='last-output'>1919 *</div>
+                            <div className='current-output'>810810810810810810810810810</div>
+                        </div>
+                        <button className='button-ac'>AC</button>
+                        <button>Del</button>
+                        <button>/</button>
+                        <button>7</button>
+                        <button>8</button>
+                        <button>9</button>
+                        <button>*</button>
+                        <button>4</button>
+                        <button>5</button>
+                        <button>6</button>
+                        <button>-</button>
+                        <button>1</button>
+                        <button>2</button>
+                        <button>3</button>
+                        <button>+</button>
+                        <button>0</button>
+                        <button>.</button>
+                        <button className='button-equal'>=</button>
+                    </div>
                 </Base>
             </React.Fragment>
         );
